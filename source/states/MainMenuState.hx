d6d14627562de311002c21f99e652421f3672856
Yugisgiy <hetgegeg@gmail.com>
2025-03-02 17:33:02 +0300

    Add files via upload

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -2,7 +2,6 @@ package states;
 
 import flixel.FlxObject;
 import flixel.addons.transition.FlxTransitionableState;
-import flixel.addons.display.FlxBackdrop;
 import flixel.effects.FlxFlicker;
 import lime.app.Application;
 import states.editors.MasterEditorMenu;
@@ -18,15 +17,12 @@ class MainMenuState extends MusicBeatState
 	var optionShit:Array<String> = [
 		'story_mode',
 		'freeplay',
-		//#if MODS_ALLOWED 'mods', #end
-		//#if ACHIEVEMENTS_ALLOWED 'awards', #end
+		#if MODS_ALLOWED 'mods', #end
+		#if ACHIEVEMENTS_ALLOWED 'awards', #end
 		'credits',
-		//#if !switch 'donate', #end
+		#if !switch 'donate', #end
 		'options'
 	];
-
-	var char:FlxSprite;
-	var backdrop:FlxBackdrop;
 
 	var magenta:FlxSprite;
 	var camFollow:FlxObject;
@@ -57,31 +53,13 @@ class MainMenuState extends MusicBeatState
 		bg.screenCenter();
 		add(bg);
 
-		var theBG:BGSprite = new BGSprite('greenfarm', -680, -130, 0, 0);
-		add(theBG);
-		
-
-		backdrop = new FlxBackdrop(Paths.image('backd'), XY, 0, 0);
-		backdrop.velocity.set(200, 110);
-		backdrop.updateHitbox();
-		backdrop.alpha = 0.5;
-		backdrop.screenCenter(X);
-		add(backdrop);
-
-		var bga:FlxSprite = new FlxSprite(-120).loadGraphic(Paths.image('bgthing'));
-		bga.setGraphicSize(Std.int(bg.width * 1.175));
-		bga.updateHitbox();
-		bga.screenCenter();
-		bga.antialiasing = ClientPrefs.globalAntialiasing;
-		add(bga);
-
 		camFollow = new FlxObject(0, 0, 1, 1);
 		add(camFollow);
 
 		magenta = new FlxSprite(-80).loadGraphic(Paths.image('menuDesat'));
 		magenta.antialiasing = ClientPrefs.data.antialiasing;
 		magenta.scrollFactor.set(0, yScroll);
-		//magenta.setGraphicSize(Std.int(magenta.width * 1.175));
+		magenta.setGraphicSize(Std.int(magenta.width * 1.175));
 		magenta.updateHitbox();
 		magenta.screenCenter();
 		magenta.visible = false;
@@ -100,7 +78,6 @@ class MainMenuState extends MusicBeatState
 			menuItem.animation.addByPrefix('idle', optionShit[i] + " basic", 24);
 			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
 			menuItem.animation.play('idle');
-			menuItem.ID = i;
 			menuItems.add(menuItem);
 			var scr:Float = (optionShit.length - 4) * 0.135;
 			if (optionShit.length < 6)
@@ -108,10 +85,9 @@ class MainMenuState extends MusicBeatState
 			menuItem.scrollFactor.set(0, scr);
 			menuItem.updateHitbox();
 			menuItem.screenCenter(X);
-			menuItem.x += 300;
 		}
 
-		var psychVer:FlxText = new FlxText(12, FlxG.height - 44, 0, "Strident Engine v" + psychEngineVersion, 12);
+		var psychVer:FlxText = new FlxText(12, FlxG.height - 44, 0, "Psych Engine v" + psychEngineVersion, 12);
 		psychVer.scrollFactor.set();
 		psychVer.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(psychVer);
@@ -119,10 +95,6 @@ class MainMenuState extends MusicBeatState
 		fnfVer.scrollFactor.set();
 		fnfVer.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(fnfVer);
-		var watermarkMenuTxt:FlxText = new FlxText(12, FlxG.height - 64, 0, "Strident Crisis V1.5", 12);
-		watermarkMenuTxt.scrollFactor.set();
-		watermarkMenuTxt.setFormat("Comic Sans MS", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		add(watermarkMenuTxt);
 		changeItem();
 
 		#if ACHIEVEMENTS_ALLOWED
@@ -136,66 +108,11 @@ class MainMenuState extends MusicBeatState
 		#end
 		#end
 
-		addTouchPad("UP_DOWN", "A_B");
+		addTouchPad("UP_DOWN", "A_B_E");
 
 		super.create();
 
-		//FlxG.camera.follow(camFollow, null, 9);
-
-		switch (FlxG.random.int(1, 6))
-		{
-			case 1:
-				char = new FlxSprite(100, 270).loadGraphic(Paths.image('mainmenu/bambiRemake'));//put your cords and image here
-				char.frames = Paths.getSparrowAtlas('mainmenu/bambiRemake');//here put the name of the xml
-				char.animation.addByPrefix('idleR', 'bambi idle', 24, true);//on 'idle normal' change it to your xml one
-				char.animation.play('idleR');//you can rename the anim however you want to
-				char.scrollFactor.set();
-				add(char);
-			case 2:
-				char = new FlxSprite(-700, -170).loadGraphic(Paths.image('mainmenu/Bamb'));//put your cords and image here
-				char.frames = Paths.getSparrowAtlas('mainmenu/Bamb');//here put the name of the xml
-				char.animation.addByPrefix('idleR', 'Bamb Idle', 24, true);//on 'idle normal' change it to your xml one
-				char.animation.play('idleR');//you can rename the anim however you want to
-				char.scrollFactor.set();
-				char.scale.set(0.6, 0.6);
-				add(char);
-			case 3:
-				char = new FlxSprite(-100, 120).loadGraphic(Paths.image('mainmenu/Banbi'));//put your cords and image here
-				char.frames = Paths.getSparrowAtlas('mainmenu/Banbi');//here put the name of the xml
-				char.animation.addByPrefix('idleR', 'Banbi Idle', 24, true);//on 'idle normal' change it to your xml one
-				char.animation.play('idleR');//you can rename the anim however you want to
-				char.scrollFactor.set();
-				char.scale.set(0.7, 0.7);
-				add(char);
-
-				case 4:
-					char = new FlxSprite(100, 140).loadGraphic(Paths.image('mainmenu/OppositionX_Assets'));//put your cords and image here
-					char.frames = Paths.getSparrowAtlas('mainmenu/OppositionX_Assets');//here put the name of the xml
-					char.animation.addByPrefix('idleR', 'Idle', 24, true);//on 'idle normal' change it to your xml one
-					char.animation.play('idleR');//you can rename the anim however you want to
-					char.scrollFactor.set();
-					char.scale.set(1.5, 1.5);
-					add(char);
-
-					case 5:
-						char = new FlxSprite(50, -80).loadGraphic(Paths.image('mainmenu/Cheater'));//put your cords and image here
-						char.frames = Paths.getSparrowAtlas('mainmenu/Cheater');//here put the name of the xml
-						char.animation.addByPrefix('idleR', 'Cheater Idle', 24, true);//on 'idle normal' change it to your xml one
-						char.animation.play('idleR');//you can rename the anim however you want to
-						char.scrollFactor.set();
-						char.scale.set(0.7, 0.7);
-						add(char);
-					case 6:
-						char = new FlxSprite(-300, -170).loadGraphic(Paths.image('mainmenu/diambi'));//put your cords and image here
-						char.frames = Paths.getSparrowAtlas('mainmenu/diambi');//here put the name of the xml
-						char.animation.addByPrefix('idleR', 'diambi idle', 24, true);//on 'idle normal' change it to your xml one
-						char.animation.play('idleR');//you can rename the anim however you want to
-						char.scrollFactor.set();
-						char.scale.set(0.7, 0.7);
-						add(char);
-					    
-
-		}
+		FlxG.camera.follow(camFollow, null, 9);
 	}
 
 	var selectedSomethin:Bool = false;
@@ -240,28 +157,6 @@ class MainMenuState extends MusicBeatState
 
 					FlxFlicker.flicker(menuItems.members[curSelected], 1, 0.06, false, false, function(flick:FlxFlicker)
 					{
-
-					menuItems.forEach(function(spr:FlxSprite)
-						{
-							if (curSelected != spr.ID)
-							{
-																FlxTween.tween(char, {x: -700}, 2, {ease: FlxEase.backInOut, type: ONESHOT, onComplete: function(twn:FlxTween) {
-									char.kill(); /*I killed the char*/
-								}});
-								FlxTween.tween(spr, {x: 1200}, 2, {ease: FlxEase.backInOut, type: ONESHOT, onComplete: function(twn:FlxTween) {
-									spr.kill(); /*Mom I killed the sprs again*/
-								}});
-								FlxTween.tween(spr, {alpha: 0}, 1.3, {ease: FlxEase.backInOut, type: ONESHOT, onComplete: function(twn:FlxTween){
-									spr.kill(); /*Mom I killed the sprs again*/
-								}});
-							}
-							else
-							{
-
-						FlxFlicker.flicker(spr, 1, 0.06, false, false, function(flick:FlxFlicker)
-
-						{
-																	   
 						switch (optionShit[curSelected])
 						{
 							case 'story_mode':
@@ -307,7 +202,10 @@ class MainMenuState extends MusicBeatState
 					}
 				}
 			}
-			MusicBeatState.switchState(new MasterEditorMenu());
+			else if (controls.justPressed('debug_1') || touchPad.buttonE.justPressed)
+			{
+				selectedSomethin = true;
+				MusicBeatState.switchState(new MasterEditorMenu());
 			}
 		}
 
@@ -317,10 +215,9 @@ class MainMenuState extends MusicBeatState
 	function changeItem(huh:Int = 0)
 	{
 		FlxG.sound.play(Paths.sound('scrollMenu'));
-		
 		menuItems.members[curSelected].animation.play('idle');
-		menuItems.offset[curSelected].y = 0;
 		menuItems.members[curSelected].updateHitbox();
+		menuItems.members[curSelected].screenCenter(X);
 
 		curSelected += huh;
 
@@ -330,10 +227,8 @@ class MainMenuState extends MusicBeatState
 			curSelected = menuItems.length - 1;
 
 		menuItems.members[curSelected].animation.play('selected');
-		camFollow.setPosition[curSelected](menuItems.getGraphicMidpoint().x, menuItems.getGraphicMidpoint().y);
-		menuItems.offset[curSelected].x = 0.15 * (menuItems.frameWidth / 2 + 180);
-		menuItems.offset[curSelected].y = 0.15 * menuItems.frameHeight;
-		FlxG.log[curSelected].add(menuItems.frameWidth);
+		menuItems.members[curSelected].centerOffsets();
+		menuItems.members[curSelected].screenCenter(X);
 
 		camFollow.setPosition(menuItems.members[curSelected].getGraphicMidpoint().x,
 			menuItems.members[curSelected].getGraphicMidpoint().y - (menuItems.length > 4 ? menuItems.length * 8 : 0));
